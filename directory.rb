3973b5729fc0fcd54a9b305d9ce72c21d15a7cdc
guspowell <augustuspowell@me.com>
2014-12-03 17:20:40 +0000

    User now has option to add country and cohort

--- directory.rb
+++ directory.rb
@@ -3,6 +3,30 @@ def print_header
 	puts "--------------"
 end
 
+def input_students
+	puts "please enter the names of the student you wish to add"
+	puts "when you are done press enter twice"
+	students = []
+	number = 1
+	name = gets.chomp
+
+	# while the name is not empty, repeat this code
+	while !name.empty? do
+    	puts "what cohort is #{name} in?"
+    	cohort = gets.chomp
+		puts "what is #{name}'s country of origin?"    	
+    	country = gets.chomp
+    	students << {:number => number, :name => name, :cohort => cohort, :country => country}
+    	puts "now we have #{students.length} great students"
+    	# get another name from user
+    	puts "please enter another name:"
+    	number += 1
+    	name = gets.chomp
+    end
+    # return the array of students
+    students
+end
+
 def print(students)
 	students.each do |student|
 		if student[:name].length < 12
@@ -13,36 +37,6 @@ end
 
 def print_footer(names)
 	puts "overall we have #{names.length} great students"
-end
-
-def input_students
-	puts "please enter the names of the student you wish to add"
-	puts "when you are done press enter twice"
-	students = []
-	# get the first name
-	number = 1
-	name = gets.chomp
-	puts "what cohort is #{name} in?"
-	cohort = gets.chomp
-	puts "what is #{name}'s country of origin?"
-	country = gets.chomp
-
-	# while the name is not empty, repeat this code
-	while !name.empty? do
-    	# add the student hash to the array
-    	students << {:number => number, :name => name, :cohort => cohort, :country => country}
-    	puts "now we have #{students.length} great students"
-    	# get another name from user
-    	puts "please enter another name:"
-    	number += 1
-    	name = gets.chomp
-    	puts "what cohort is #{name} in?"
-    	cohort = gets.chomp
-		puts "what is #{name}'s country of origin?"    	
-    	country = gets.chomp
-    end
-    # return the array of students
-    students
 end
 
 
