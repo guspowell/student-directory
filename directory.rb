bd476517627a22897c22b3cd12451638d2f747cd
guspowell <augustuspowell@me.com>
2014-12-07 16:13:33 +0000

    changed File to CSV.foreach in load option. Also added unused method for select csv file

--- directory.rb
+++ directory.rb
@@ -1,5 +1,9 @@
 @students = [] # an empty array accessible to all methods
 
+def add_to_array(number, name, cohort, country)
+	@students << {:number => number, :name => name, :cohort => cohort, :country => country}
+end
+
 def print_header
 	puts "The students of my cohort at Makers Academy".center(100)
 	puts "--------------".center(100)
@@ -8,16 +12,15 @@ end
 def input_students
 	puts "please enter the names of the student you wish to add"
 	puts "when you are done press enter twice"
-	number = 1
+	number = @students.length + 1
 	name = STDIN.gets.chomp
-
 	# while the name is not empty, repeat this code
 	while !name.empty? do
     	puts "what cohort is #{name} in?"
     	cohort = STDIN.gets.chomp
 		puts "what is #{name}'s country of origin?"    	
     	country = STDIN.gets.chomp
-    	@students << {:number => number, :name => name, :cohort => cohort, :country => country}
+    	add_to_array(number, name, cohort, country)
     	if @students.length == 1
     		puts "now we have #{@students.length} great student"
     	else
@@ -55,6 +58,7 @@ def print_menu
 	puts "2. Show the students"
 	puts "3. Save the list to students.csv"
 	puts "4. Load students from students.csv"
+	puts "5. Check source code"
 	puts "9. Exit"
 end
 
@@ -74,8 +78,10 @@ def process(selection)
   		save_students
   	when "4"
   		load_students
+  	when "5"
+  		source_code
   	when "9"
-  		Exit
+  		exit
   	else
   		puts "I don't know what you meant, try again"
   	end
@@ -88,32 +94,42 @@ def interactive_menu
   	end
 end
 
+def csv_file_list # in current directory
+	csv_files = Dir.entries(".")
+	csv_files.select! { |file| file.include?("csv") }
+	puts csv_files
+end
+
+def select_csv_file # from current directory
+	puts "what file would you like to select:"
+	csv_file_list
+	filename = gets.chomp
+end
+
 def save_students
-	# open the file for writing
-	file = File.open("students.csv", "w")
-	# iterate over the array of students
-	@students.each do |student|
-		student_data = [student[:number], student[:name], student[:cohort], student[:country]]
-		csv_line = student_data.join (",")
-		file.puts csv_line
+	File.open("students.csv", "w") do |file|
+		@students.each do |student|
+			student_data = [student[:number], student[:name], student[:cohort], student[:country]]
+			csv_line = student_data.join (",")
+			file.puts csv_line
+		end
 	end
-	file.close
 	puts "saved"
 end
 
 def load_students(filename = "students.csv")
-	file = File.open(filename, "r")
-	file.readlines.each do |line|
-		number, name, cohort, country = line.chomp.split(',')
-		@students << {:number => number, :name => name, :cohort => cohort, :country => country}
+	require 'csv'
+	CSV.foreach(filename) do |line|
+		number, name, cohort, country = line
+		add_to_array(number, name, cohort, country)
 	end
-	file.close
+	puts " #{filename} has been loaded"
 end
 
 def try_load_students
 	filename = ARGV.first #first argument from the command
 	return if filename.nil? #get out of the method if filename isn't given
-	if File.exists(filename)
+	if File.exists?(filename)
 		load_students(filename)
 		puts "loaded #{@students.length} from #{filename}"
 	else # if it doesn't exist
@@ -122,6 +138,11 @@ def try_load_students
 	end
 end
 
+def source_code
+	puts $0 # or can use __FILE__
+end
+
+try_load_students
 interactive_menu
 
 
